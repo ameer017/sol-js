e7d54030ae9b8217b327492c0e06cf601b178c0c
ameer017 <rajiabdullahi907@gmail.com>
2024-02-08 14:08:54 +0100

    new files

--- Blockchain.js
+++ Blockchain.js
@@ -1,17 +1,17 @@
-const SHA256 = require('sha256');
+// const SHA256 = require('sha256');
 
-console.log(SHA256("man").toString());
+// console.log(SHA256("man").toString());
 
-let hash = "";
-let nonce = 0;
+// let hash = "";
+// let nonce = 0;
 
-while (hash.substring(0,3) !== "000") {
+// while (hash.substring(0,3) !== "000") {
 
-    nonce++;
+//     nonce++;
     
-    hash = SHA256("man" + nonce).toString()
-}
+//     hash = SHA256("man" + nonce).toString()
+// }
 
-console.log(nonce);
-console.log("\n");
-console.log(hash)
+// console.log(nonce);
+// console.log("\n");
+// console.log(hash)
