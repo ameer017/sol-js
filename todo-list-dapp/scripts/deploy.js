c493fb14f7e43c2e6af42ce70fe7bca50526b4fb
ameer017 <rajiabdullahi907@gmail.com>
2024-04-08 12:29:34 +0100

    contract_done

---
+++ todo-list-dapp/scripts/deploy.js
@@ -0,0 +1,12 @@
+const hre = require("hardhat");
+
+async function main() {
+  const todolist = await hre.ethers.deployContract("ToDoListApp");
+  await todolist.waitForDeployment();
+  console.log(`Cupcake vending machine deployed to ${todolist.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
